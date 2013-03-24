864b1ca189c61fdc653137e99cd20215b7d62d6e
neerja <neerja.libra@gmail.com>
2013-03-24 13:57:38 +0530

    Request backend

--- pages/roomMateRequest.php
+++ pages/roomMateRequest.php
@@ -1,3 +1,11 @@
 <?php
-echo "adadsa";   
+$checkId = mysql_real_escape_string($_POST['roomMate1']);
+if($checkId){
+$query = "UPDATE userPreferences SET roommateId = ".$checkId;
+mysql_query($query);
+date_default_timezone_set('Asia/Calcutta');
+  echo date("l jS of F g:i A.", time());   
+
+  }
+
 ?>
