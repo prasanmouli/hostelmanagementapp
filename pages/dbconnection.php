2df43b71b76d28f6e197a74e5cf552ccc460695f
rapidwein <rapidwein@gmail.com>
2013-03-26 20:32:29 +0530

    refined search

--- pages/dbconnection.php
+++ pages/dbconnection.php
@@ -1,10 +1,15 @@
 <?php
 include_once 'config.lib.php';
+$userId = $_SESSION['userId'];
+$userId = "100000";
+$query = "SELECT year FROM userDetails WHERE userId = ".$userId;
+$info = mysql_fetch_array(mysql_query($query));
+
 $searchq = mysql_real_escape_string($_GET['name']);
 echo $searchq;
 if($searchq!=NULL&&$searchq!=""){
-$getname_sql = "SELECT rollNo,userName FROM userDetails WHERE userName LIKE '".$searchq."%'";
-$getname1_sql = "SELECT rollNo,userName FROM userDetails WHERE rollNo LIKE '".$searchq."%'";
+$getname_sql = "SELECT rollNo,userName FROM userDetails WHERE userName LIKE '".$searchq."%' AND year = ".$info['year'];
+$getname1_sql = "SELECT rollNo,userName FROM userDetails WHERE rollNo LIKE '".$searchq."%' AND year = ".$info['year'];
 $getName = mysql_query($getname_sql);
 while ($row = mysql_fetch_array($getName)) {
 echo $row['userName'] ." ".$row['rollNo']. '<br/>';
