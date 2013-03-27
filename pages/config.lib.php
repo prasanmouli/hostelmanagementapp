6605d080a803dbb5122cce99e6440d0fbd80d40a
Prasanna <prasanmouli@gmail.com>
2013-03-27 19:48:49 +0530

    dbchanges

--- pages/config.lib.php
+++ pages/config.lib.php
@@ -1,13 +1,11 @@
 <?php
 /* DEFINITIONS to connect to the database */
-//ip address of the server.
+
 define("SERVER","localhost");
-//the username for connection.
-define("USERNAME","Hostel2013");
-//the password used for connection.
-define("SERVER_PASSWORD","Hajime123");
+define("USERNAME","");
+define("SERVER_PASSWORD","");
 //name of the database.
-define("DATABASE","Hostel2013");
+define("DATABASE","");
 
 include_once("connection.php");
 
