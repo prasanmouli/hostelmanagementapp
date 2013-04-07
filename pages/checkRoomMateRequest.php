75dae316b6cf1235ad53fc717969287624e8afc9
rapidwein <rapidwein@gmail.com>
2013-04-07 18:20:08 +0530

    refined input field

--- pages/checkRoomMateRequest.php
+++ pages/checkRoomMateRequest.php
@@ -3,7 +3,7 @@ include_once "config.lib.php";
 $userId = $_SESSION['userId'];
 $userId = "100000";
  $data = "";    
-$query = " SELECT * FROM requests WHERE userId = " . $userId ; 
+$query = " SELECT * FROM requests WHERE userId = " . $userId." AND accepted IS NULL OR accepted=1" ; 
 $res = mysql_query($query);
 while($info = mysql_fetch_array($res)){
       $query_init = "SELECT rollNo FROM userDetails WHERE userId = '".$info['roomMateRequestId']."'";
