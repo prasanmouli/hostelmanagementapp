cda237c0cdd23e60d801631644ac1c1a343d4621
neerja <neerja.libra@gmail.com>
2013-04-08 00:12:23 +0530

    randomize floorpreferred rooms

--- pages/allotment.php
+++ pages/allotment.php
@@ -1,9 +1,10 @@
 <?php
 include_once("config.lib.php");
-$groupId = 1; 
+$groupId = 48; 
 $capacity;                    
 $counter=0;                         
-          
+$noOfRoomsPerFloor=10;
+$noOfFloors=3;          
 $queryHostelPref = "SELECT preference FROM finalRoomList WHERE groupId =".$groupId;
 $res = mysql_query($queryHostelPref);
 while($info = mysql_fetch_array($res)){
@@ -18,7 +19,7 @@ $array1[]= $capacityNumber;
 for($i=1;$i<=60;$i++)
   $array1[$i] = $i;
 $rand_arr=array_rand($array1,1);
-print_r($rand_arr);
+//print_r($rand_arr);
 unset($array1[$rand_arr]);
 if(ctype_alpha($pref)) {
 
@@ -29,9 +30,21 @@ $qur1 = "UPDATE finalRoomList SET roomNo=".$rand_arr
 }
 
 
- else {
+else {
+  $l=($pref+1)*$noOfRoomsPerFloor;
+  //echo $l.".";
+  $const=$l-$noOfRoomsPerFloor;
+  
+  for($k=$l;$k>$const;$k--)
+    $array2[$k] = $k;
+     
+  $rand_arr1=array_rand($array2,1);
+  print_r($rand_arr1);
+  //unset($array2[$rand_arr1]);
+$qur2 = "UPDATE finalRoomList SET roomNo=".$rand_arr1
+  ." WHERE groupId = ".$groupId;
+mysql_query($qur2);
 
-   // echo $pref;
 }
 
 ?>
